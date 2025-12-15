ea3c77dbf8659b3bb08f4aa89e58dccf57636f71
oskie5802 <minoroskar2009@gmail.com>
2025-12-15 14:30:42 +0100

    fix v3 dla tego gowna na macos

--- flake.nix
+++ flake.nix
@@ -7,59 +7,58 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    naersk.url = "github:nix-community/naersk/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixos-generators, naersk, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixos-generators, flake-utils, ... }:
 
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
 
-      # Budujemy VM tylko dla x86_64-linux
-      vm-for-x86_64 = nixos-generators.nixosGenerate {
+      # Budujemy VM tylko dla x86_64-linux (do cache'a lub remote build)
+      vmPackage = nixos-generators.nixosGenerate {
         system = "x86_64-linux";
         format = "vm";
         modules = [ ./configuration.nix ];
       };
 
+      # Skrypt uruchamiający VM – działa na KAŻDYM systemie (w tym aarch64-darwin)
+      runVmScript = pkgs.writeScriptBin "run-omnios-vm" ''
+        #!${pkgs.runtimeShell}
+
+        echo "Uruchamiam OmniOS VM (x86_64) przez QEMU na ${system}..."
+
+        # Probujemy użyć gotowego wyniku z cache'a Nix (jeśli ktoś wcześniej zbudował)
+        # lub z lokalnego store, jeśli jest
+        if [ -f "./result/bin/run-nixos-vm" ]; then
+          exec ./result/bin/run-nixos-vm "$@"
+        fi
+
+        # Jeśli nie ma lokalnie – próbujemy pobrać z cache'a (cachix lub oficjalny)
+        echo "Pobieram pre-built VM z cache'a Nix..."
+        ${pkgs.nix}/bin/nix shell ${self}#packages.x86_64-linux.run-vm --impure --command run-nixos-vm "$@"
+      '';
+
     in {
       packages = {
-        # Oryginalne pakiety tylko dla x86_64-linux
-        x86_64-linux.run-vm = vm-for-x86_64;
+        # Dla x86_64-linux – oryginalne zachowanie
+        x86_64-linux.run-vm = vmPackage;
         x86_64-linux.install-iso = nixos-generators.nixosGenerate {
           system = "x86_64-linux";
           format = "install-iso";
           modules = [ ./configuration.nix ];
         };
 
-        # Nowy pakiet działający na KAŻDYM systemie (w tym aarch64-darwin)
-        run-vm-cross = pkgs.writeScriptBin "run-omnios-vm" ''
-          #!${pkgs.runtimeShell}
-
-          # Ścieżka do zbudowanego skryptu VM (z x86_64-linux)
-          VM_SCRIPT=${vm-for-x86_64}/bin/run-nixos-vm
-
-          if [ ! -f "$VM_SCRIPT" ]; then
-            echo "VM nie jest jeszcze zbudowana. Buduję teraz (to może potrwać)..."
-            ${pkgs.nix}/bin/nix build ${self}#packages.x86_64-linux.run-vm --extra-experimental-features "nix-command flakes"
-            VM_SCRIPT=./result/bin/run-nixos-vm
-          fi
-
-          echo "Uruchamiam OmniOS VM przez QEMU..."
-          exec "$VM_SCRIPT" "$@"
-        '';
-
-        # Domyślny pakiet/app na wszystkich platformach
-        default = self.packages.${system}.run-vm-cross;
+        # Dostępne na wszystkich platformach
+        run-vm-cross = runVmScript;
+        default = runVmScript;
       };
 
       apps = {
         default = {
           type = "app";
-          program = "${self.packages.${system}.run-vm-cross}/bin/run-omnios-vm";
+          program = "${runVmScript}/bin/run-omnios-vm";
         };
-
         run-vm = self.apps.${system}.default;
       };
     });
